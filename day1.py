a2aa289f355d2b92342e8ba1a3d1384f3d2b081d
knehj <jonathan.knehr@kkr.com>
2022-12-01 16:55:50 -0500

    automate submission!

--- day1.py
+++ day1.py
@@ -1,8 +1,13 @@
-import requests
+import aocd
 
-cookie = ''
-ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+session = ''
 
-x = requests.get('https://adventofcode.com/2022/day/1/input', headers={'Cookie': cookie, 'User-Agent': ua}, verify=False)
-sums = [sum([int(x) for x in elf if x != '']) for elf in map(lambda v: v.split('\n'), x.text.split('\n\n'))]
-print(max(sums), sum(sorted(sums, reverse=True)[:3]))
+x = aocd.get_data(session, day=1, year=2022)
+
+sums = [sum([int(x) for x in elf if x != '']) for elf in map(lambda v: v.split('\n'), x.split('\n\n'))]
+a = max(sums)
+b = sum(sorted(sums, reverse=True)[:3])
+
+aocd.submit(a, part='a', day=1, year=2022, session=session)
+aocd.submit(b, part='b', day=1, year=2022, session=session)
+
