cf98f40bade12d28d3c1458fb4658014077e9ab3
knehj <jonathan.knehr@kkr.com>
2022-12-01 17:11:47 -0500

    automate submission!

--- day1.py
+++ day1.py
@@ -1,13 +1,12 @@
 import aocd
 
-session = ''
-
-x = aocd.get_data(session, day=1, year=2022)
+x = aocd.get_data(day=1, year=2022)
 
 sums = [sum([int(x) for x in elf if x != '']) for elf in map(lambda v: v.split('\n'), x.split('\n\n'))]
 a = max(sums)
 b = sum(sorted(sums, reverse=True)[:3])
+print(a, b)
 
-aocd.submit(a, part='a', day=1, year=2022, session=session)
-aocd.submit(b, part='b', day=1, year=2022, session=session)
+aocd.submit(a, part='a', day=1, year=2022)
+aocd.submit(b, part='b', day=1, year=2022)
 
